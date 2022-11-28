e370806398a0469eb32adf38489fef7ab1234a92
Deny <flutterlabz@gmail.com>
2022-11-28 18:11:56 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:example/core.dart';
 import 'package:example/session.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
@@ -21,15 +22,17 @@ var t = TrController();
 void main() async {
   await initialize();
   // windows, android, ios
-  var path = await getTemporaryDirectory();
-  Hive.init(path.path);
+  if (!kIsWeb) {
+    var path = await getTemporaryDirectory();
+    Hive.init(path.path);
+  }
   mainStorage = await Hive.openBox('mainStorage');
 
   await UserService.load();
   await ThemeService.load();
   await LocalProductService.load();
   await FormHistoryService.load();
-  AppSession.token = await mainStorage.get("token");
+  AppSession.token = await mainStorage.get("token") ?? "";
 
   // print("mainStorage.isOpen: ${mainStorage.isOpen}");
   // print("storage: ${await mainStorage.get("token")}");
